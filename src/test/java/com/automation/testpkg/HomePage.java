3800fcac468be771796c347a3e5173bcc4757a14
srinivast2018 <srinivas.nirmal@gmail.com>
2020-04-03 12:16:01 +0530

    second commit changed the code

---
+++ src/test/java/com/automation/testpkg/HomePage.java
@@ -0,0 +1,14 @@
+package com.automation.testpkg;
+
+import org.testng.annotations.Test;
+
+public class HomePage {
+	
+	@Test
+	public void homePage()
+	{
+		System.out.println("Its Home Page");
+		
+	}
+
+}
--- src/test/java/com/automation/testpkg/LaunchApp.java
+++ src/test/java/com/automation/testpkg/LaunchApp.java
@@ -13,6 +13,7 @@ public class LaunchApp {
 		WebDriver driver=new ChromeDriver();
 		
 		driver.get("https://www.facebook.com");
+		driver.manage().window().maximize();
 	}	
 	
 	
