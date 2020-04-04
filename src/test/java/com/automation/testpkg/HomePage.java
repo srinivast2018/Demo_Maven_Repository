d5b1afc8db22c253680e6dabef7c820a3c534922
srinivast2018 <srinivas.nirmal@gmail.com>
2020-04-04 15:46:40 +0530

    Changed Home Page code

--- src/test/java/com/automation/testpkg/HomePage.java
+++ src/test/java/com/automation/testpkg/HomePage.java
@@ -4,10 +4,12 @@ import org.testng.annotations.Test;
 
 public class HomePage {
 	
+		
 	@Test
 	public void homePage()
 	{
 		System.out.println("Its Home Page");
+		System.out.println("End of Home Page");
 		
 	}
 
